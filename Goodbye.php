33285a2ff0d585c96a0e3c26b5029995855d7e20
Kilian <kilian.mader@hotmail.de>
2023-08-09 22:00:56 +0200

    add subclass 'Social' which extends class 'Hello' and add a function called 'bye' which returns the correct farewell regarding the language

--- Goodbye.php
+++ Goodbye.php
@@ -14,6 +14,16 @@ class Hello
         if (strtolower($this->lang) == "en") return "Hello";
     }
 }
+class Social extends Hello
+{
+    function bye()
+    {
+        if (strtolower($this->lang) == "fr") return "Au revoir!";
+        if (strtolower($this->lang) == "es") return "Adios!";
+
+        return "goodbye!";
+    }
+}
 
 
 
