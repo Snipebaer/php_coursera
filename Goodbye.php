c7dbb4d2f98c25fb2b5d07be9fdf606e269ee922
Kilian <kilian.mader@hotmail.de>
2023-08-09 21:56:22 +0200

    add goodbye.php file and copied the code of  Hello class from the file 'Hello.php' into it

---
+++ Goodbye.php
@@ -0,0 +1,20 @@
+<?php
+class Hello
+{
+    protected $lang;
+
+    function __construct($lang)
+    {
+        $this->lang = $lang;
+    }
+
+    function greet()
+    {
+        if (strtolower($this->lang) == "fr") return "Bonjour";
+        if (strtolower($this->lang) == "en") return "Hello";
+    }
+}
+
+
+
+?>
