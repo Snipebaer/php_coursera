52040e32322facc27fdf4717485c7e96da51c0d8
Kilian <kilian.mader@hotmail.de>
2023-08-09 22:12:32 +0200

    add code outsid the class to demonstrate the access on the variables regarding the access modifiers

--- Visibility.php
+++ Visibility.php
@@ -14,6 +14,10 @@ class MyClass
     }
 }
 
+$obj = new MyClass();
 
-
+echo $obj->pub . "</br>";
+//echo $obj->pro . "</br>"; // wont work cause its protected
+//echo $obj->priv . "</br>"; // wont work cause its private
+echo $obj->printHello(); // works fine because the method was definied inside the class
 ?>
